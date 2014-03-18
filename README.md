37fe4c9cc3f25db51f2e410a7941f19317c1e5d2
Asbjørn Enge <asbjorn@hanafjedle.net>
2014-03-18 14:39:36 +0100

    Testing

--- README.md
+++ README.md
@@ -4,4 +4,12 @@ Shameless clone of [websocketd](https://github.com/joewalnes/websocketd) written
 
 Mostly cause I needed a *"missing feature"* (less security) in websocketd. Didn't want to wait. Didn't want to write any Go. And it's like 20 lines of code...
 
+## Build
+
+	docker run -v $(pwd):/app -w /app asbjornenge/nexe-docker -o websocketd.js
+
+## Run
+
+	tail -f file.log | websocketd.js
+
 enjoy.
--- index.js
+++ index.js
@@ -1,9 +1,43 @@
-var WebSocketServer = require('ws').Server
-  , wss = new WebSocketServer({port: 8080});
+// var WebSocketServer = require('ws').Server
+//   , wss = new WebSocketServer({port: 8080});
+
+// wss.broadcast = function(data) {
+//     for(var i in this.clients)
+//         this.clients[i].send(data);
+// };
+
+var http = require('http');
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+server.listen(8080, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
+
+var WebSocketServer = require('websocket').server;
+wss = new WebSocketServer({
+     httpServer: server,
+    // You should not use autoAcceptConnections for production
+    // applications, as it defeats all standard cross-origin protection
+    // facilities built into the protocol and the browser.  You should
+    // *always* verify the connection's origin and decide whether or not
+    // to accept it.
+    autoAcceptConnections: true
+});
+
+wss.on('request', function(request) {
+    var connection = request.accept('echo-protocol', request.origin);
+    console.log((new Date()) + ' Connection accepted.');
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+});
 
 wss.broadcast = function(data) {
-    for(var i in this.clients)
-        this.clients[i].send(data);
+    for(var i in this.connections)
+        this.connections[i].send(data);
 };
 
 process.stdin.setEncoding('utf8');
