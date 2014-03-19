78111c34bbd3495393098ed685640928198d8817
Asbjørn Enge <asbjorn@hanafjedle.net>
2014-03-19 11:46:42 +0100

    Rewrote spawning child for each connection with stdin support

--- index.js
+++ index.js
@@ -1,31 +1,52 @@
-var ws = require('nodejs-websocket')
+/** websocketd.js
+ *
+ * STDOUT into a websocket server
+ *
+ */
+
+var _     = require('lodash')
+var ws    = require('nodejs-websocket')
+var argv  = require('optimist').argv
+var spawn = require('child_process').spawn
+
+var default_config = {
+    port : null,
+    host : '0.0.0.0'
+}
+
+var config    = _.merge(_.clone(default_config, true), argv)
+var proc      = _.reduce(process.argv, function(was, key) {
+    if (key === config._[0]) this.cmd = true
+    return this.cmd ? was.concat(key) : was
+},[])
 
 var server = ws.createServer(function (conn) {
     console.log("New connection")
-    conn.on("text", function (str) {
-        console.log("Received "+str)
-        conn.sendText(str.toUpperCase()+"!!!")
+
+    conn.proc = spawn(proc[0],proc.slice(1))
+    conn.proc.stdout.on('data', function (data) {
+        conn.sendText(data)
+    });
+    conn.proc.stderr.on('data', function (data) {
+        conn.sendText(data)
+    });
+    conn.proc.on('error', function(err) {
+        conn.proc = null
+    })
+
+    conn.on("text", function(data) {
+        if (!conn.proc) return
+        try { conn.proc.stdin.write(data) }
+        catch (err) { console.log('ERROR: '+err); conn.sendText('ERROR: '+err); conn.proc = null }
     })
     conn.on("close", function (code, reason) {
         console.log("Connection closed")
+        conn.proc.kill('SIGHUP')
     })
-}).listen(8080)
-
-server.broadcast = function(data) {
-    for(var i in this.connections)
-        this.connections[i].sendText(data);
-}
-
-process.stdin.setEncoding('utf8');
-
-process.stdin.on('readable', function(chunk) {
-  var chunk = process.stdin.read();
-  if (chunk !== null) {
-    server.broadcast(chunk)
-    process.stdout.write('data: ' + chunk);
-  }
-});
-
-process.stdin.on('end', function() {
-  process.stdout.write('end');
-});
+    conn.on("error", function(err) {
+        console.log("Error",err)
+        conn.proc.kill('SIGHUP')
+    })
+}).listen(config.port, config.host, function() {
+    console.log('Broadcasting @',this.socket.address())
+})
